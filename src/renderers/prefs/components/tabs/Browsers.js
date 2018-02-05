9ed1480a9e693eee422d64a6624548414857760f
Will Stone <will@wstone.io>
2018-02-05 22:08:39 +0000

    add checkbox

--- src/renderers/prefs/components/tabs/Browsers.js
+++ src/renderers/prefs/components/tabs/Browsers.js
@@ -1,5 +1,5 @@
 import { ipcRenderer } from 'electron'
-// import { css } from 'emotion'
+import { css } from 'emotion'
 import React from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
@@ -21,6 +21,8 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   userSelect: 'none',
   padding: '1rem',
   margin: `0 0 0.5rem 0`,
+  display: 'flex',
+  alignItems: 'center',
 
   // change background colour if dragging
   background: isDragging ? '#11151B' : '#21252B',
@@ -91,10 +93,10 @@ class Browsers extends React.Component {
               style={getListStyle(snapshot.isDraggingOver)}
             >
               {this.props.browsers &&
-                this.props.browsers.map((item, index) => (
+                this.props.browsers.map((browser, index) => (
                   <Draggable
-                    key={item.name}
-                    draggableId={item.name}
+                    key={browser.name}
+                    draggableId={browser.name}
                     index={index}
                   >
                     {(provided, snapshot) => (
@@ -107,10 +109,43 @@ class Browsers extends React.Component {
                             provided.draggableProps.style
                           )}
                         >
-                          <span {...provided.dragHandleProps}>
+                          <span
+                            {...provided.dragHandleProps}
+                            className={css`
+                              opacity: 0.5;
+                            `}
+                          >
                             &#8942;&#8942;
-                          </span>{' '}
-                          <span>{item.name}</span>
+                          </span>
+                          <img
+                            src={`../../images/browser-logos/${
+                              browser.name
+                            }.png`}
+                            alt=""
+                            className={css`
+                              width: 32px;
+                              height: 32px;
+                              vertical-align: middle;
+                              margin-left: 1rem;
+                              margin-right: 1rem;
+                            `}
+                          />
+                          <span>{browser.name}</span>
+                          <div
+                            className="pretty p-svg"
+                            style={{ marginLeft: 'auto' }}
+                          >
+                            <input type="checkbox" checked={browser.enabled} />
+                            <div className="state p-success">
+                              <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                <path
+                                  d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"
+                                  style={{ stroke: 'white', fill: 'white' }}
+                                />
+                              </svg>
+                              <label />
+                            </div>
+                          </div>
                         </div>
                         {provided.placeholder}
                       </div>
