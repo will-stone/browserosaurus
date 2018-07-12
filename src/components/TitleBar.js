050ab07aa8548d67d3855035545a83bb0e8abc5a
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-12 18:44:47 +0100

    animate picker (#66)
    * feat: scale-n-fade animation for picker show/hide
    
    * pin eslint-scope (older versions compromised)

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,11 +1,11 @@
 import styled from 'react-emotion'
 
 const TitleBar = styled('div')`
-  height: 2rem;
+  height: 4rem;
   -webkit-app-region: drag;
   text-align: center;
-  line-height: 2rem;
-  margin-bottom: 1rem;
+  line-height: 4rem;
+  padding: 0 1rem;
 `
 
 export default TitleBar
