61090a4680131a5254e8b16f08fd49a8d6d5c758
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-17 18:03:11 +0100

    tests: tests running

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
+const ResizeObserver = require('resize-observer-polyfill')
+
 module.exports = {
+  globals: {
+    ResizeObserver,
+  },
+  moduleNameMapper: {
+    electron: '<rootDir>/mocks/electron.js',
+  },
   testPathIgnorePatterns: ['/build/', '/docs/', '/out/'],
 }
---
+++ mocks/electron.js
@@ -0,0 +1,3 @@
+export const ipcRenderer = {
+  on: jest.fn(),
+}
