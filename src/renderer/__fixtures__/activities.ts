0318eadf748d8f4d023cf703535f4fa58f3654b1
Will Stone <654103+will-stone@users.noreply.github.com>
2019-06-16 09:53:39 +0100

    fix: type check failing on fixtures
    closes #107

--- src/renderer/__fixtures__/activities.ts
+++ src/renderer/__fixtures__/activities.ts
@@ -1,3 +1,12 @@
+import braveDev from 'browser-logos/src/brave-dev/brave-dev_256x256.png'
+import brave from 'browser-logos/src/brave/brave_256x256.png'
+import googleChrome from 'browser-logos/src/chrome/chrome_256x256.png'
+import chromium from 'browser-logos/src/chromium/chromium_256x256.png'
+import firefoxDevEdition from 'browser-logos/src/firefox-developer-edition/firefox-developer-edition_256x256.png'
+import firefoxNightly from 'browser-logos/src/firefox-nightly/firefox-nightly_256x256.png'
+import firefox from 'browser-logos/src/firefox/firefox_256x256.png'
+import opera from 'browser-logos/src/opera/opera_256x256.png'
+import safari from 'browser-logos/src/safari/safari_256x256.png'
 import { Activity } from '../../model'
 
 export const activities2: Activity[] = [
@@ -6,11 +15,13 @@ export const activities2: Activity[] = [
     cmd: 'open "{URL}" -a Firefox',
     hotKey: 'f',
     name: 'Firefox',
+    logo: firefox,
   },
   {
     cmd: 'open "{URL}" -a Safari',
     hotKey: 's',
     name: 'Safari',
+    logo: safari,
   },
 ]
 
@@ -20,23 +31,27 @@ export const activities4: Activity[] = [
     cmd: 'open "{URL}" -a Firefox',
     hotKey: 'f',
     name: 'Firefox',
+    logo: firefox,
   },
   {
     cmd: 'open "{URL}" -a Safari',
     hotKey: 's',
     name: 'Safari',
+    logo: safari,
   },
   {
     appId: 'Google Chrome',
     cmd: 'open "{URL}" -a "Google Chrome"',
     hotKey: 'g',
     name: 'Google Chrome',
+    logo: googleChrome,
   },
   {
     appId: 'Opera',
     cmd: 'open "{URL}" -a Opera',
     hotKey: 'o',
     name: 'Opera',
+    logo: opera,
   },
 ]
 
@@ -46,29 +61,34 @@ export const activities5: Activity[] = [
     cmd: 'open "{URL}" -a Brave-Browser-Dev',
     hotKey: 'r',
     name: 'Brave Dev',
+    logo: braveDev,
   },
   {
     appId: 'Brave Browser',
     cmd: 'open "{URL}" -a "Brave Browser"',
     hotKey: 'b',
     name: 'Brave Browser',
+    logo: brave,
   },
   {
     appId: 'Chromium',
     cmd: 'open "{URL}" -a Chromium',
     hotKey: 'c',
     name: 'Chromium',
+    logo: chromium,
   },
   {
     appId: 'Firefox',
     cmd: 'open "{URL}" -a Firefox',
     hotKey: 'f',
     name: 'Firefox',
+    logo: firefox,
   },
   {
     cmd: 'open "{URL}" -a Safari',
     hotKey: 's',
     name: 'Safari',
+    logo: safari,
   },
 ]
 
@@ -78,41 +98,48 @@ export const activities7: Activity[] = [
     cmd: 'open "{URL}" -a Brave-Browser-Dev',
     hotKey: 'r',
     name: 'Brave Dev',
+    logo: braveDev,
   },
   {
     appId: 'Brave Browser',
     cmd: 'open "{URL}" -a "Brave Browser"',
     hotKey: 'b',
     name: 'Brave Browser',
+    logo: brave,
   },
   {
     appId: 'Chromium',
     cmd: 'open "{URL}" -a Chromium',
     hotKey: 'c',
     name: 'Chromium',
+    logo: chromium,
   },
   {
     appId: 'Firefox',
     cmd: 'open "{URL}" -a Firefox',
     hotKey: 'f',
     name: 'Firefox',
+    logo: firefox,
   },
   {
     cmd: 'open "{URL}" -a Safari',
     hotKey: 's',
     name: 'Safari',
+    logo: safari,
   },
   {
     appId: 'Google Chrome',
     cmd: 'open "{URL}" -a "Google Chrome"',
     hotKey: 'g',
     name: 'Google Chrome',
+    logo: googleChrome,
   },
   {
     appId: 'Opera',
     cmd: 'open "{URL}" -a Opera',
     hotKey: 'o',
     name: 'Opera',
+    logo: opera,
   },
 ]
 
@@ -122,52 +149,61 @@ export const activities9: Activity[] = [
     cmd: 'open "{URL}" -a Brave-Browser-Dev',
     hotKey: 'r',
     name: 'Brave Dev',
+    logo: braveDev,
   },
   {
     appId: 'Brave Browser',
     cmd: 'open "{URL}" -a "Brave Browser"',
     hotKey: 'b',
     name: 'Brave Browser',
+    logo: brave,
   },
   {
     appId: 'Chromium',
     cmd: 'open "{URL}" -a Chromium',
     hotKey: 'c',
     name: 'Chromium',
+    logo: chromium,
   },
   {
     appId: 'Firefox',
     cmd: 'open "{URL}" -a Firefox',
     hotKey: 'f',
     name: 'Firefox',
+    logo: firefox,
   },
   {
     appId: 'Firefox Developer Edition',
     cmd: 'open "{URL}" -a "Firefox Developer Edition"',
     hotKey: 'd',
     name: 'Firefox Developer Edition',
+    logo: firefoxDevEdition,
   },
   {
     appId: 'Firefox Nightly',
     cmd: 'open "{URL}" -a "Firefox Nightly"',
     hotKey: 'n',
     name: 'Firefox Nightly',
+    logo: firefoxNightly,
   },
   {
     cmd: 'open "{URL}" -a Safari',
     hotKey: 's',
     name: 'Safari',
+    logo: safari,
   },
   {
     appId: 'Google Chrome',
     cmd: 'open "{URL}" -a "Google Chrome"',
     hotKey: 'g',
     name: 'Google Chrome',
+    logo: googleChrome,
   },
   {
     appId: 'Opera',
     cmd: 'open "{URL}" -a Opera',
     hotKey: 'o',
     name: 'Opera',
+    logo: opera,
   },
 ]
