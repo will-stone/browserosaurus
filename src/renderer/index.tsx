63cd65cd173bc4cc9d198021f4c09f1a977b4900
Will Stone <654103+will-stone@users.noreply.github.com>
2019-11-01 08:55:09 +0000

    cleanup: remove last of styled components

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,41 +1,13 @@
+import './index.css'
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { createGlobalStyle } from 'styled-components'
 
 import App from './App'
 import { ErrorBoundary } from './ErrorBoundary'
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    user-select: none !important;
-    box-sizing: border-box;
-  }
-
-  body {
-    background-color: transparent;
-    margin: 0;
-    overflow: hidden;
-    font-family: 
-      -apple-system,
-      system-ui,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      'Helvetica Neue',
-      Arial,
-      sans-serif;
-  }
-
-  html,
-  body,
-  #picker-root {
-    height: 100%;
-  }
-`
-
 ReactDOM.render(
   <ErrorBoundary>
-    <GlobalStyle />
     <App />
   </ErrorBoundary>,
   document.getElementById('picker-root'),
