c65ec549f429aa90f5d472005e8606f117bb16b8
Will Stone <654103+will-stone@users.noreply.github.com>
2020-05-28 21:05:21 +0100

    Improve browser button layout

--- src/renderer/components/the-browser-buttons.tsx
+++ src/renderer/components/the-browser-buttons.tsx
@@ -8,21 +8,27 @@ import BrowserButton from './browser-button'
 
 const TheBrowserButtons: React.FC = () => {
   const browsers: Browser[] = useRecoilValue(browsersAtom)
-  const remainder = browsers.length % 5
+
+  const threeCols = browsers.length <= 3 || browsers.length === 6
+  const fourCols =
+    browsers.length === 4 ||
+    browsers.length === 7 ||
+    browsers.length === 8 ||
+    browsers.length === 11 ||
+    browsers.length === 12
+  const fiveCols =
+    browsers.length === 5 ||
+    browsers.length === 9 ||
+    browsers.length === 10 ||
+    browsers.length >= 13
 
   return (
     <div
       className={cc([
         'grid gap-4',
-        { 'grid-cols-3': browsers.length <= 3 || remainder === 1 },
-        {
-          'grid-cols-4':
-            browsers.length === 4 || (browsers.length > 5 && remainder === 2),
-        },
-        {
-          'grid-cols-5':
-            browsers.length >= 5 && (remainder === 0 || remainder >= 3),
-        },
+        { 'grid-cols-3': threeCols },
+        { 'grid-cols-4': fourCols },
+        { 'grid-cols-5': fiveCols },
       ])}
     >
       {browsers.map((browser) => (
