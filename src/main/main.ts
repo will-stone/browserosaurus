dad10237c487ce85890e6d9436af13a79fdf2473
Will Stone <654103+will-stone@users.noreply.github.com>
2020-05-29 15:42:06 +0100

    Downgrade electron to fix signing issue

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,6 +19,9 @@ import createWindow from './createWindow'
 import { BROWSERS_SCANNED, URL_HISTORY_CHANGED } from './events'
 import { store } from './store'
 
+// TODO [electron@>=9] This will be the default in Electron 9, remove once upgraded
+app.allowRendererProcessReuse = true
+
 // Attempt to fix this bug: https://github.com/electron/electron/issues/20944
 app.commandLine.appendArgument('--enable-features=Metal')
 
