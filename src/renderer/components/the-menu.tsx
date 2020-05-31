1a7cfe73779ff2faac8ab0d57f1b8f993b6cb37f
Will Stone <654103+will-stone@users.noreply.github.com>
2020-05-31 11:15:29 +0100

    Make menu appear above content

--- src/renderer/components/the-menu.tsx
+++ src/renderer/components/the-menu.tsx
@@ -18,7 +18,7 @@ const TheMenu: React.FC = () => {
       className="absolute top-0 left-0 right-0 p-1"
       style={{ bottom: '-16px' }}
     >
-      <div className="w-full h-full bg-grey-900 rounded overflow-y-auto overflow-x-hidden">
+      <div className="w-full h-full bg-grey-800 rounded overflow-y-auto overflow-x-hidden border border-grey-300 shadow-xl">
         <div className="p-4 grid grid-cols-2 gap-4">
           <div>
             <h2 className="text-grey-500 mb-2 font-semibold">Favourite</h2>
@@ -29,13 +29,12 @@ const TheMenu: React.FC = () => {
                   <label
                     key={browser.id}
                     className={cc([
-                      'block p-2 space-x-4',
-                      { 'bg-grey-800': isStriped },
+                      'block py-2 px-3 space-x-2 rounded',
+                      { 'bg-grey-700': isStriped },
                     ])}
                   >
                     <input
                       checked={favBrowserId === browser.id}
-                      className="align-text-top"
                       name="browser-fav"
                       onChange={handleFavChange}
                       type="radio"
