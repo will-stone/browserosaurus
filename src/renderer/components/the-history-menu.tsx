9325e8e97ad049b41fde9a035aceaaf803e2864e
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-01 16:44:19 +0100

    Move history menu in from right

--- src/renderer/components/the-history-menu.tsx
+++ src/renderer/components/the-history-menu.tsx
@@ -10,7 +10,7 @@ const TheHistoryMenu: React.FC = () => {
   return (
     <div
       className="absolute bg-grey-800 rounded overflow-y-auto overflow-x-hidden border border-grey-600 shadow-xl z-30"
-      style={{ top: '60px', right: '123px', bottom: '8px', left: '24px' }}
+      style={{ top: '60px', right: '140px', bottom: '8px', left: '24px' }}
     >
       <div className="p-4">
         {urlHistory
