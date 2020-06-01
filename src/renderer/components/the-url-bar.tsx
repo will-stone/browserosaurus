88c1f0ad56c8cfcac55e204e5507e636ad123780
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-01 16:58:53 +0100

    Make all of URL bar open the history menu

--- src/renderer/components/the-url-bar.tsx
+++ src/renderer/components/the-url-bar.tsx
@@ -27,12 +27,18 @@ const TheUrlBar: React.FC<Props> = ({ className }) => {
   }, [openMenu, setOpenMenu])
 
   return (
-    <div className={cc([className, 'flex items-center space-x-4'])}>
-      <div
+    <div
+      className={cc([
+        className,
+        'flex items-center space-x-4',
+        { 'opacity-50': !parsedUrl },
+      ])}
+    >
+      <button
         className={cc([
           'flex-grow',
           'bg-grey-700',
-          'border rounded-full',
+          'border rounded-full focus:outline-none',
           { 'border-grey-900': !openMenu },
           { 'border-grey-600': openMenu === 'history' },
           'shadow-inner',
@@ -41,74 +47,73 @@ const TheUrlBar: React.FC<Props> = ({ className }) => {
           'flex items-center justify-between',
           'overflow-hidden',
           { 'bg-grey-300 z-30': openMenu === 'history' },
+          'cursor-default',
         ])}
+        disabled={!parsedUrl}
+        onClick={handleUrlHistoryOpenClick}
+        type="button"
       >
         {parsedUrl ? (
-          <>
-            <div className="flex items-center space-x-2 truncate">
-              <ProtocolIcon
-                className="flex-shrink-0"
-                urlProtocol={parsedUrl.protocol}
-              />
-              <div className="truncate">
-                <span className="font-bold text-grey-200 text-sm">
-                  {parsedUrl.hostname}
-                </span>
-                <span>
-                  {parsedUrl.port && `:${parsedUrl.port}`}
-                  {parsedUrl.pathname}
-                  {parsedUrl.search}
-                  {parsedUrl.hash}
-                </span>
-              </div>
+          <div className="flex items-center space-x-2 truncate">
+            <ProtocolIcon
+              className="flex-shrink-0"
+              urlProtocol={parsedUrl.protocol}
+            />
+            <div className="truncate">
+              <span className="font-bold text-grey-200 text-sm">
+                {parsedUrl.hostname}
+              </span>
+              <span>
+                {parsedUrl.port && `:${parsedUrl.port}`}
+                {parsedUrl.pathname}
+                {parsedUrl.search}
+                {parsedUrl.hash}
+              </span>
             </div>
-
-            <button
-              className={cc([
-                'flex-shrink-0',
-                'flex items-center justify-center',
-                'w-6 h-6',
-                'border border-grey-900 rounded-full focus:outline-none',
-                { 'bg-grey-600 text-grey-300': openMenu !== 'history' },
-                { 'bg-grey-300 text-grey-800': openMenu === 'history' },
-                'cursor-default',
-              ])}
-              onClick={handleUrlHistoryOpenClick}
-              type="button"
-            >
-              <svg
-                aria-hidden="true"
-                className={cc([
-                  'w-2 h-2 transform',
-                  { 'rotate-180': openMenu === 'history' },
-                ])}
-                focusable="false"
-                role="img"
-                viewBox="0 0 448 512"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
-          </>
+          </div>
         ) : (
-          <span className="text-grey-500">
-            Most recently clicked link will show here
-          </span>
+          <span>Most recently clicked link will show here</span>
         )}
-      </div>
+
+        <div
+          className={cc([
+            'flex-shrink-0',
+            'flex items-center justify-center',
+            'w-6 h-6',
+            'border border-grey-900 rounded-full',
+            { 'bg-grey-600 text-grey-300': openMenu !== 'history' },
+            { 'bg-grey-300 text-grey-800': openMenu === 'history' },
+          ])}
+        >
+          <svg
+            aria-hidden="true"
+            className={cc([
+              'w-2 h-2 transform',
+              { 'rotate-180': openMenu === 'history' },
+            ])}
+            focusable="false"
+            role="img"
+            viewBox="0 0 448 512"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+      </button>
 
       <button
         className={cc([
           'bg-grey-700',
-          'border border-grey-900 rounded shadow-md active:shadow-none focus:outline-none',
-          'text-xs active:text-grey-200 font-bold',
+          'border border-grey-900 rounded shadow-md focus:outline-none',
+          'text-xs font-bold',
+          { 'active:text-grey-200 active:shadow-none': parsedUrl },
           'py-2 px-2 space-x-2',
           'cursor-default',
         ])}
+        disabled={!parsedUrl}
         onClick={handleCopyClick}
         type="button"
       >
