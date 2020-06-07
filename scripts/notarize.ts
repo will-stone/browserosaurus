6745e8b2b1a6f0b3dfdfcfbb804927d47600cfb7
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-07 19:52:05 +0100

    Fix unable to notarize

--- scripts/notarize.ts
+++ scripts/notarize.ts
@@ -1,13 +1,12 @@
-import { notarize } from 'electron-notarize'
-
+const notarize = require('electron-notarize')
 const projectRoot = require('path').resolve(__dirname, '..')
 
-notarize({
+notarize.notarize({
   appBundleId: 'com.browserosaurus',
   appPath: `${projectRoot}/out/Browserosaurus-darwin-x64/Browserosaurus.app`,
   appleId: String(process.env.APPLE_ID),
   appleIdPassword: '@keychain:AC_PASSWORD',
   ascProvider: 'Z89KPMLTFR',
-}).catch((error) => {
+}).catch((error: Error) => {
   console.error(`Didn't work :( ${error.message}`) // eslint-disable-line no-console
 })
