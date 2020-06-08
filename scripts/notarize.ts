6c0ff2e778ee8169737472f6062b9c0017ef7704
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-08 11:18:04 +0100

    Refactor notarize to use dotenv

--- scripts/notarize.ts
+++ scripts/notarize.ts
@@ -1,5 +1,8 @@
+import { config } from 'dotenv'
 import { notarize } from 'electron-notarize'
 import path from 'path'
+
+config()
 
 notarize({
   appBundleId: 'com.browserosaurus',
@@ -10,7 +13,11 @@ notarize({
   ),
   appleId: String(process.env.APPLE_ID),
   appleIdPassword: '@keychain:AC_PASSWORD',
+  // Team ID
   ascProvider: 'Z89KPMLTFR',
 }).catch((error) => {
-  console.error(`Didn't work :( ${error.message}`) // eslint-disable-line no-console
+  // eslint-disable-next-line no-console
+  console.error("Notarization didn't work :(", error.message)
+  // eslint-disable-next-line unicorn/no-process-exit
+  process.exit(1)
 })
