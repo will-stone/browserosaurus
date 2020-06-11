485047246f9d582b2c229a2cc54e77097654cdcb
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-11 14:42:03 +0100

    Use version js to updated docs

---
+++ docs/version.js
@@ -0,0 +1,6 @@
+const version = '10.5.0'
+
+const downloadButton = document.querySelector('#js-download-button')
+downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/download/v${version}/Browserosaurus-${version}.dmg`
+
+downloadButton.innerHTML = `Download Browserosaurus v${version} for Mac`
