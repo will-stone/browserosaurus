c38ecfdab52d6dfbee39a2fc6e598e5c35043699
Will Stone <654103+will-stone@users.noreply.github.com>
2020-06-21 18:22:02 +0100

    Docs 10.9.0

--- docs/version.js
+++ docs/version.js
@@ -1,4 +1,4 @@
-const version = '10.7.1'
+const version = '10.9.0'
 
 const downloadButton = document.querySelector('#js-download-button')
 downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/download/v${version}/Browserosaurus-${version}.dmg`
