b64c43689ce34c1487a32f1d8b25a7d5b912bb24
Will Stone <email@wstone.io>
2020-07-09 22:14:11 +0100

    Break-out appExists func

--- src/utils/doesAppExist.ts
+++ src/utils/doesAppExist.ts
@@ -5,9 +5,18 @@ import { App } from '../config/types'
 
 const execFileP = promisify(execFile)
 
-export async function doesAppExist(app: App): Promise<boolean> {
-  const { stdout: appPath } = await execFileP('mdfind', [
-    `kMDItemContentType == 'com.apple.application-bundle' && kMDItemCFBundleIdentifier == '${app.id}'`,
-  ])
+async function appExists(nameOrBundleId: string): Promise<boolean> {
+  const isBundleId = nameOrBundleId.includes('.')
+
+  const query = isBundleId
+    ? `kMDItemContentType == 'com.apple.application-bundle' && kMDItemCFBundleIdentifier == '${nameOrBundleId}'`
+    : `kMDItemKind == 'Application' && kMDItemFSName == '${nameOrBundleId}.app'c`
+
+  const { stdout: appPath } = await execFileP('mdfind', [query])
+
   return Boolean(appPath)
+}
+
+export function doesAppExist(app: App): Promise<boolean> {
+  return appExists(app.id)
 }
