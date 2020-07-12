3de4afc7b7ca17732b6c85763f4ff17121b24424
Will Stone <email@wstone.io>
2020-07-12 14:16:27 +0100

    Tweak docs wording

--- docs/version.js
+++ docs/version.js
@@ -3,4 +3,4 @@ const version = '10.11.1'
 const downloadButton = document.querySelector('#js-download-button')
 downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/download/v${version}/Browserosaurus-${version}.dmg`
 
-downloadButton.innerHTML = `Download Browserosaurus v${version} for Mac`
+downloadButton.innerHTML = `Download Browserosaurus v${version}`
