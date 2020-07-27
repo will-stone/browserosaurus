dffa16bab6a5377c66326a6337a3eb0c81c845c9
Will Stone <email@wstone.io>
2020-07-27 14:41:26 +0100

    Replace fa pro with fa free

--- src/renderer/components/menu__sponsor.tsx
+++ src/renderer/components/menu__sponsor.tsx
@@ -1,4 +1,4 @@
-import { faHeart } from '@fortawesome/pro-solid-svg-icons/faHeart'
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import cc from 'classcat'
 import React, { useCallback } from 'react'
