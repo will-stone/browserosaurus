f645e9113ddc4027aecc3a6de2e26a0dba103f4d
Will Stone <email@wstone.io>
2020-09-06 21:56:34 +0100

    Docs v11.1.0

--- docs/version.js
+++ docs/version.js
@@ -1,4 +1,4 @@
-const version = '11.0.1'
+const version = '11.1.0'
 
 const downloadButton = document.querySelector('#js-download-button')
 downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/download/v${version}/Browserosaurus-${version}.dmg`
