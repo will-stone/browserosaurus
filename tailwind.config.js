0d055956591883200ec863c9009a725ef2c318dd
Will Stone <email@wstone.io>
2020-09-14 13:38:11 +0100

    Remove unused custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,25 +20,8 @@ module.exports = {
       sans: 'SF Pro Text, Helvetica, Arial, sans-serif',
       rounded: 'SF Pro Rounded, Helvetica, Arial, sans-serif',
     },
-    extend: {
-      colors: {
-        'grey-200': '#C6CCD7',
-        'grey-300': '#A9B2C3',
-        'grey-400': '#848C9B',
-        'grey-500': '#5F6672',
-        'grey-600': '#2F333B',
-        'grey-700': '#212428',
-        'grey-800': '#1A1D21',
-        'grey-900': '#0D1117',
-      },
-      spacing: {
-        '28': '7rem',
-      },
-    },
   },
   variants: {
-    backgroundColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
     boxShadow: ['responsive', 'hover', 'focus', 'active'],
     opacity: ['responsive', 'hover', 'focus', 'active'],
   },
