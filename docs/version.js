f59a036e384d8ee185e2b336e9a323fc5d04ef30
Will Stone <654103+will-stone@users.noreply.github.com>
2020-11-27 08:23:51 +0000

    Put cask install inline

--- docs/version.js
+++ docs/version.js
@@ -4,13 +4,3 @@ const downloadButton = document.querySelector('#js-download-button')
 downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/download/v${version}/Browserosaurus-${version}.dmg`
 
 downloadButton.innerHTML = `Download Browserosaurus v${version}`
-
-const homebrewCaskCommand = 'brew install browserosaurus --cask'
-const homebrewCaskButton = document.querySelector('#js-homebrew-cask-button')
-homebrewCaskButton.addEventListener('click', function () {
-  navigator.clipboard.writeText(homebrewCaskCommand)
-})
-
-$().ready(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
