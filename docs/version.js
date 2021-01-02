5cc95b8513a9bfc6569dabbaed284dd7e71a2ee0
Will Stone <654103+will-stone@users.noreply.github.com>
2021-01-02 22:58:43 +0000

    Remove docs

--- docs/version.js
+++
@@ -1,7 +0,0 @@
-// const version = '12.3.1'
-
-const downloadButton = document.querySelector('#js-download-button')
-// TODO figure out how to detect arch
-downloadButton.href = `https://github.com/will-stone/browserosaurus/releases/latest`
-
-downloadButton.innerHTML = `Download Browserosaurus`
