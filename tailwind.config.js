c6162cf3adcc76b6a7abf75be5cf1c3437849b48
Will Stone <654103+will-stone@users.noreply.github.com>
2021-04-11 13:45:24 +0100

    Bump linting config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
   purge: false,
   theme: {
     fontSize: {
-      xxs: '.6rem',
-      xs: '.75rem',
-      sm: '.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
+      'xxs': '.6rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
       '2xl': '1.5rem',
       '3xl': '1.875rem',
       '4xl': '2.25rem',
