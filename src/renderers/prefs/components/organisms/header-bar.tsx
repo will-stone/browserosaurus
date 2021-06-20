accd751cf83accc47b8abe92b1c61887945206be
Will Stone <654103+will-stone@users.noreply.github.com>
2021-06-20 10:42:32 +0100

    Update tiles prefs header

--- src/renderers/prefs/components/organisms/header-bar.tsx
+++ src/renderers/prefs/components/organisms/header-bar.tsx
@@ -1,19 +1,13 @@
 import clsx from 'clsx'
 import React from 'react'
 
-import { useSelector } from '../../../../shared/state/hooks'
-
 interface Props {
   className?: string
 }
 
 export const HeaderBar = ({ className }: Props): JSX.Element => {
-  const isDarkMode = useSelector((state) => state.theme.isDarkMode)
-
   return (
-    <div
-      className={clsx(isDarkMode ? 'bg-gray-700' : 'bg-gray-300', className)}
-    >
+    <div className={clsx(className)}>
       <div
         className={clsx('flex justify-center items-center', 'h-8', 'draggable')}
       >
