41f16fc375f4577fab498f317d08d9864ed437b7
Will Stone <654103+will-stone@users.noreply.github.com>
2021-08-22 08:44:22 +0100

    Fix empty apps list

--- src/main/state/thunk.get-installed-app-ids.ts
+++ src/main/state/thunk.get-installed-app-ids.ts
@@ -1,3 +1,5 @@
+import sleep from 'tings/sleep'
+
 import { apps } from '../../config/apps'
 import { syncAppIds } from '../../shared/state/actions'
 import type { AppThunk } from '../../shared/state/reducer.root'
@@ -5,5 +7,13 @@ import { filterAppsByInstalled } from '../utils/filter-apps-by-installed'
 
 export const getInstalledAppIds = (): AppThunk => async (dispatch) => {
   const installedApps = await filterAppsByInstalled(apps)
-  dispatch(syncAppIds(installedApps))
+  // It appears that sometimes the installed app IDs are not fetched, maybe a
+  // race with Spotlight index? So if none found, keep retrying.
+  // https://github.com/will-stone/browserosaurus/issues/425
+  if (installedApps.length === 0) {
+    await sleep(500)
+    dispatch(getInstalledAppIds())
+  } else {
+    dispatch(syncAppIds(installedApps))
+  }
 }
