8e921adaa9b58c35031592f05fe37fc35db08e41
Will Stone <654103+will-stone@users.noreply.github.com>
2022-01-01 13:19:07 +0000

    refactor: namespace tray logs away from main

--- src/main/state/actions.ts
+++ src/main/state/actions.ts
@@ -6,27 +6,31 @@ import type { Data } from '../../shared/state/reducer.data'
 import type { Storage } from '../../shared/state/reducer.storage'
 import { actionNamespacer } from '../../shared/utils/action-namespacer'
 
-const cA = actionNamespacer('main')
+const main = actionNamespacer('main')
 
-const readiedApp = cA('app/readied')
+const readiedApp = main('app/readied')
 
-const openedUrl = cA<string>('url/opened')
+const openedUrl = main<string>('url/opened')
 
-const changedPickerWindowBounds = cA<Rectangle>('picker-window-bounds/changed')
+const changedPickerWindowBounds = main<Rectangle>(
+  'picker-window-bounds/changed',
+)
 
-const installedAppsRetrieved = cA<AppId[]>('installed-apps/retrieved')
+const installedAppsRetrieved = main<AppId[]>('installed-apps/retrieved')
 
 const syncReducers =
-  cA<CombinedState<{ data: Data; storage: Storage }>>('sync-reducers')
+  main<CombinedState<{ data: Data; storage: Storage }>>('sync-reducers')
 
-const gotDefaultBrowserStatus = cA<boolean>('default-browser-status/got')
+const gotDefaultBrowserStatus = main<boolean>('default-browser-status/got')
 
-const availableUpdate = cA('update/available')
-const downloadingUpdate = cA('update/downloading')
-const downloadedUpdate = cA('update/downloaded')
+const availableUpdate = main('update/available')
+const downloadingUpdate = main('update/downloading')
+const downloadedUpdate = main('update/downloaded')
 
-const clickedRestorePicker = cA('restore-picker/clicked')
-const clickedOpenPrefs = cA('open-prefs/clicked')
+const tray = actionNamespacer('tray')
+
+const clickedRestorePicker = tray('restore-picker/clicked')
+const clickedOpenPrefs = tray('open-prefs/clicked')
 
 export {
   availableUpdate,
