5a844662abde6c38a880955f432b85bf97dd28aa
Will Stone <654103+will-stone@users.noreply.github.com>
2022-06-12 08:34:17 +0100

    refactor: move main native_modules rules

--- webpack.main.config.cjs
+++ webpack.main.config.cjs
@@ -9,7 +9,26 @@ module.exports = {
   entry: './src/main/main.ts',
   // Put your normal webpack config below here
   module: {
-    rules,
+    rules: [
+      {
+        // We're specifying native_modules in the test because the asset
+        // relocator loader generates a "fake" .node file which is really
+        // a cjs file.
+        test: /native_modules\/.+\.node$/u,
+        use: 'node-loader',
+      },
+      {
+        test: /\.(m?js|node)$/u,
+        parser: { amd: false },
+        use: {
+          loader: '@vercel/webpack-asset-relocator-loader',
+          options: {
+            outputAssetBase: 'native_modules',
+          },
+        },
+      },
+      ...rules,
+    ],
   },
   // Do not create source maps
   devtool: false,
