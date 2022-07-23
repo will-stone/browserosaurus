651138aed4bd549b6adac1f28748332a0822c41a
Will Stone <654103+will-stone@users.noreply.github.com>
2022-07-23 07:52:41 +0100

    refactor: change dnd lib to allow for React 18 upgrade

--- src/renderers/prefs/components/organisms/pane-apps.tsx
+++ src/renderers/prefs/components/organisms/pane-apps.tsx
@@ -1,14 +1,27 @@
-import ArrowSmDownIcon from '@heroicons/react/solid/ArrowSmDownIcon'
-import ArrowSmUpIcon from '@heroicons/react/solid/ArrowSmUpIcon'
-import SwitchVerticalIcon from '@heroicons/react/solid/SwitchVerticalIcon'
+import type { DragEndEvent } from '@dnd-kit/core'
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import clsx from 'clsx'
 import React from 'react'
-import type { DropResult } from 'react-beautiful-dnd'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { useDispatch } from 'react-redux'
 
+import type { AppId } from '../../../../config/apps'
 import Input from '../../../shared/components/atoms/input'
 import { Spinner } from '../../../shared/components/atoms/spinner'
+import type { InstalledApp } from '../../../shared/state/hooks'
 import {
   useDeepEqualSelector,
   useInstalledApps,
@@ -17,26 +30,89 @@ import {
 import { reorderedApp, updatedHotCode } from '../../state/actions'
 import { Pane } from '../molecules/pane'
 
-interface DragDirectionArrowProps {
-  currentIndex: number
-  endIndex: number
-  className: string
+interface SortableItemProps {
+  id: InstalledApp['id']
+  name: InstalledApp['name']
+  index: number
+  icon?: string
+  keyCode?: string
 }
 
-const DragDirectionArrow = ({
-  currentIndex,
-  endIndex,
-  className,
-}: DragDirectionArrowProps) => {
-  if (currentIndex === 0) {
-    return <ArrowSmDownIcon className={className} />
+const SortableItem = ({
+  id,
+  name,
+  keyCode = '',
+  index,
+  icon = '',
+}: SortableItemProps) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id })
+
+  const dispatch = useDispatch()
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
   }
 
-  if (currentIndex === endIndex) {
-    return <ArrowSmUpIcon className={className} />
-  }
-
-  return <SwitchVerticalIcon className={className} />
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className={clsx(
+        'flex',
+        'bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 shadow',
+        'rounded-xl mb-4',
+        'focus-visible:outline-none focus-visible:bg-white dark:focus-visible:bg-black focus-visible:bg-opacity-70 focus-visible:shadow-xl focus-visible:ring-1 focus-visible:ring-gray-500',
+        isDragging &&
+          'focus-visible:ring-2 dark:focus-visible:ring-gray-100 focus-visible:ring-gray-900',
+      )}
+    >
+      <div className="flex items-center justify-center p-4 w-16">
+        {index + 1}
+      </div>
+      <div className="flex-grow flex items-center p-4">
+        <img
+          alt=""
+          className={clsx('h-8 w-8 mr-4', !icon && 'hidden')}
+          src={icon}
+        />
+        <span>{name}</span>
+      </div>
+      <div className="p-4 flex items-center justify-center">
+        <Input
+          aria-label={`${name} hotkey`}
+          className="h-8 w-12"
+          data-app-id={id}
+          maxLength={1}
+          minLength={0}
+          onChange={(event) => event.preventDefault()}
+          onFocus={(event) => {
+            event.target.select()
+          }}
+          onKeyPress={(event) => {
+            dispatch(
+              updatedHotCode({
+                appId: id,
+                value: event.code,
+              }),
+            )
+          }}
+          placeholder="Key"
+          type="text"
+          value={keyCode}
+        />
+      </div>
+    </div>
+  )
 }
 
 export function AppsPane(): JSX.Element {
@@ -44,21 +120,22 @@ export function AppsPane(): JSX.Element {
 
   const installedApps = useInstalledApps()
 
-  const onDragEnd = (result: DropResult) => {
-    if (!result.destination) {
-      return
-    }
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  )
 
-    if (result.destination.index === result.source.index) {
-      return
+  const onDragEnd = ({ active, over }: DragEndEvent) => {
+    if (active.id !== over?.id) {
+      dispatch(
+        reorderedApp({
+          sourceId: active.id as AppId,
+          destinationId: over?.id as AppId,
+        }),
+      )
     }
-
-    dispatch(
-      reorderedApp({
-        sourceId: installedApps[result.source.index].id,
-        destinationId: installedApps[result.destination.index].id,
-      }),
-    )
   }
 
   const icons = useDeepEqualSelector((state) => state.data.icons)
@@ -73,88 +150,29 @@ export function AppsPane(): JSX.Element {
         </div>
       )}
 
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(droppableProvided) => (
-            <div
-              ref={droppableProvided.innerRef}
-              className="overflow-y-auto p-2"
-              {...droppableProvided.droppableProps}
-            >
-              {installedApps.map(({ id, name, hotCode }, index) => (
-                <Draggable key={id} draggableId={id} index={index}>
-                  {(draggableProvided, draggableSnapshot) => (
-                    <div
-                      ref={draggableProvided.innerRef}
-                      className={clsx(
-                        'flex',
-                        'bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 shadow',
-                        'rounded-xl mb-4',
-                        'focus-visible:outline-none focus-visible:bg-white dark:focus-visible:bg-black focus-visible:bg-opacity-70 focus-visible:shadow-xl focus-visible:ring-1 focus-visible:ring-gray-500',
-                        draggableSnapshot.isDragging &&
-                          'focus-visible:ring-2 dark:focus-visible:ring-gray-100 focus-visible:ring-gray-900',
-                      )}
-                      {...draggableProvided.draggableProps}
-                      {...draggableProvided.dragHandleProps}
-                    >
-                      <div className="flex items-center justify-center pl-4">
-                        <DragDirectionArrow
-                          className={clsx(
-                            'h-4',
-                            draggableSnapshot.isDragging
-                              ? 'text-black dark:text-white'
-                              : 'opacity-30',
-                          )}
-                          currentIndex={index}
-                          endIndex={installedApps.length - 1}
-                        />
-                      </div>
-                      <div className="flex items-center justify-center p-4 w-16">
-                        {index + 1}
-                      </div>
-                      <div className="flex-grow flex items-center p-4">
-                        <img
-                          alt=""
-                          className={clsx(
-                            'h-8 w-8 mr-4',
-                            !icons[id] && 'hidden',
-                          )}
-                          src={icons[id] || ''}
-                        />
-                        <span>{name}</span>
-                      </div>
-                      <div className="p-4 flex items-center justify-center">
-                        <Input
-                          aria-label={`${name} hotkey`}
-                          className="h-8 w-12"
-                          data-app-id={id}
-                          maxLength={1}
-                          minLength={0}
-                          onFocus={(event) => {
-                            event.target.select()
-                          }}
-                          onKeyPress={(event) => {
-                            dispatch(
-                              updatedHotCode({
-                                appId: id,
-                                value: event.code,
-                              }),
-                            )
-                          }}
-                          placeholder="Key"
-                          type="text"
-                          value={keyCodeMap[hotCode || ''] || ''}
-                        />
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {droppableProvided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <div className="overflow-y-auto p-2">
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={onDragEnd}
+          sensors={sensors}
+        >
+          <SortableContext
+            items={installedApps}
+            strategy={verticalListSortingStrategy}
+          >
+            {installedApps.map(({ id, name, hotCode }, index) => (
+              <SortableItem
+                key={id}
+                icon={icons[id]}
+                id={id}
+                index={index}
+                keyCode={keyCodeMap[hotCode || '']}
+                name={name}
+              />
+            ))}
+          </SortableContext>
+        </DndContext>
+      </div>
     </Pane>
   )
 }
