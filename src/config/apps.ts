8ec7d622f86d4f312282ec65589fe6a82ce79b1d
Stéphane FEUGA OSHIMA <sfeuga@member.fsf.org>
2022-07-26 05:45:07 +0900

    feat: add official Slack app from macOS AppStore (#558)
    Co-authored-by: Will Stone <654103+will-stone@users.noreply.github.com>

--- src/config/apps.ts
+++ src/config/apps.ts
@@ -170,6 +170,9 @@ export const apps = typeApps({
   'com.kitze.sizzy': {
     name: 'Sizzy',
   },
+  'com.tinyspeck.slackmacgap': {
+    name: 'Slack',
+  },
   'com.spotify.client': {
     name: 'Spotify',
   },
