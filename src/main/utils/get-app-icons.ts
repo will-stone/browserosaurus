a23b646bc51dff6886f68380968a865aa1cc9b14
Will Stone <654103+will-stone@users.noreply.github.com>
2022-08-12 13:23:46 +0100

    fix: icons not loading after app uninstalled

--- src/main/utils/get-app-icons.ts
+++ src/main/utils/get-app-icons.ts
@@ -9,17 +9,23 @@ import { dispatch } from '../state/store'
 
 export async function getAppIcons(apps: Storage['apps']): Promise<void> {
   try {
-    const buffers: Buffer[] = await fileIcon.buffer(
-      apps.map((app) => app.id),
-      { size: 64 },
-    )
+    const buffers: (Buffer | null)[] = []
+
+    for await (const app of apps) {
+      try {
+        const buffer = await fileIcon.buffer(app.id, { size: 64 })
+        buffers.push(buffer)
+      } catch {
+        buffers.push(null)
+      }
+    }
 
     const icons: Partial<Record<AppId, string>> = {}
 
     for (const [index, buffer] of Object.entries(buffers)) {
-      icons[apps[Number(index)].id] = `data:image/png;base64,${buffer.toString(
-        'base64',
-      )}`
+      icons[apps[Number(index)].id] = buffer
+        ? `data:image/png;base64,${buffer.toString('base64')}`
+        : ''
     }
 
     dispatch(gotAppIcons(icons))
