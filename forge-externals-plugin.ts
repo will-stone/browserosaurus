9205de68e8e30adf9a11def9675efa829703b11d
Will Stone <654103+will-stone@users.noreply.github.com>
2022-11-06 18:25:39 +0000

    chore: upgrade to electron forge 6 (stable)

---
+++ forge-externals-plugin.ts
@@ -0,0 +1,68 @@
+import { PluginBase } from '@electron-forge/plugin-base'
+import type {
+  ForgeHookMap,
+  ForgeMutatingHookFn,
+} from '@electron-forge/shared-types'
+
+interface Options {
+  externals: string[]
+}
+
+/**
+ * Required until @timfish/forge-externals-plugin updates to support getHooks
+ */
+class ForgeExternalsPlugin extends PluginBase<Options> {
+  public name = 'ForgeExternalsPlugin'
+
+  public externals: string[] = []
+
+  public constructor(options: Options) {
+    super(options)
+    this.externals = options.externals
+    this.getHooks = this.getHooks.bind(this)
+  }
+
+  public getHooks(): ForgeHookMap {
+    return {
+      resolveForgeConfig: this.resolveForgeConfig,
+    }
+  }
+
+  // eslint-disable-next-line require-await
+  private resolveForgeConfig: ForgeMutatingHookFn<'resolveForgeConfig'> =
+    async (forgeConfig) => {
+      const foundModules = new Set(this.externals)
+
+      // The webpack plugin already sets the ignore function.
+      const existingIgnoreFunction = forgeConfig.packagerConfig.ignore
+
+      // We override it and ensure we include external modules too
+      forgeConfig.packagerConfig.ignore = (file) => {
+        if (
+          typeof existingIgnoreFunction === 'function' &&
+          existingIgnoreFunction(file) === false
+        ) {
+          return false
+        }
+
+        if (file === '/node_modules') {
+          return false
+        }
+
+        for (const module of foundModules) {
+          if (
+            file.startsWith(`/node_modules/${module}`) ||
+            file.startsWith(`/node_modules/${module.split('/')[0]}`)
+          ) {
+            return false
+          }
+        }
+
+        return true
+      }
+
+      return forgeConfig
+    }
+}
+
+export default ForgeExternalsPlugin
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
+
     cssnano: {},
   },
 }
