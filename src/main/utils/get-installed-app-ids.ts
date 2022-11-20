7fed7adb581d216b22611961fc9486b8e55e681d
Will Stone <654103+will-stone@users.noreply.github.com>
2022-11-20 21:22:18 +0000

    fix: missing browsers

--- src/main/utils/get-installed-app-ids.ts
+++ src/main/utils/get-installed-app-ids.ts
@@ -1,14 +1,56 @@
+import { execFile } from 'node:child_process'
+import { homedir } from 'node:os'
+import path from 'node:path'
+import { promisify } from 'node:util'
+
 import { sleep } from 'tings'
 
+import type { AppId } from '../../config/apps'
 import { apps } from '../../config/apps'
 import { retrievedInstalledApps, startedScanning } from '../state/actions'
 import { dispatch } from '../state/store'
-import { filterAppsByInstalled } from './filter-apps-by-installed'
 
-export async function getInstalledAppIds(): Promise<void> {
+const execFileP = promisify(execFile)
+
+async function getAllInstalledBundleIds(): Promise<string[]> {
+  const { stdout: allApps } = await execFileP('mdfind', [
+    "kMDItemKind == 'Application'",
+    '-onlyin',
+    '/Applications',
+    '-onlyin',
+    path.join(homedir(), 'Applications'),
+  ])
+
+  const bundleIds = await Promise.all(
+    allApps
+      .trim()
+      .split('\n')
+      .map(async (entry) => {
+        const { stdout: bundleId } = await execFileP('mdls', [
+          '-name',
+          'kMDItemCFBundleIdentifier',
+          '-raw',
+          entry,
+        ])
+
+        return bundleId
+      }),
+  )
+
+  return bundleIds
+}
+
+async function getInstalledAppIds(): Promise<void> {
   dispatch(startedScanning())
 
-  const installedApps = await filterAppsByInstalled(apps)
+  const allInstalledBundleIds = await getAllInstalledBundleIds()
+  const installedApps: AppId[] = []
+
+  for (const installedBundleId of allInstalledBundleIds) {
+    if (installedBundleId in apps) {
+      installedApps.push(installedBundleId as AppId)
+    }
+  }
 
   // It appears that sometimes the installed app IDs are not fetched, maybe a
   // race with Spotlight index? So if none found, keep retrying.
@@ -20,3 +62,5 @@ export async function getInstalledAppIds(): Promise<void> {
     dispatch(retrievedInstalledApps(installedApps))
   }
 }
+
+export { getInstalledAppIds }
