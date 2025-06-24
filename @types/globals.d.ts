e3052b4bef4a4442cdddd1d848fb5e6aea175aa6
Andrey Paramonov <a.paramonov@dodopizza.com>
2025-06-24 17:31:58 +0300

    fix: correct Window interface declaration for electron API

--- @types/globals.d.ts
+++ @types/globals.d.ts
@@ -2,10 +2,12 @@ declare module '*.png'
 declare module '*.svg'
 declare module '*.css'
 
-interface Window {
-  electron: {
-    receive: (channel: string, callback: (...args: unknown[]) => void) => void
-    send: (channel: string, action: unknown) => void
-    getIcon: (appName: string) => Promise<string>
+declare global {
+  interface Window {
+    electron: {
+      receive: (channel: string, callback: (...args: unknown[]) => void) => void
+      send: (channel: string, action: unknown) => void
+      getIcon: (appName: string) => Promise<string>
+    }
   }
 }
